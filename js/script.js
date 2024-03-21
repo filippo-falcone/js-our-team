3d1521004241f78ea72283176aa8c13f0bde72a0
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-21 10:11:47 +0100

    feat: added two methods  to create cards

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
 // Bonus 1: trasformo la caratteristica image  in immagini nel dom
 */
 /* MATERIALS */
+
 const cardContainer = document.querySelector('#card-container');
 const team = [
     {
@@ -38,17 +39,66 @@ const team = [
         image: 'img/barbara-ramos-graphic-designer.jpg'
     }
 ];
+
 /* BODY */
+
 for (let i = 0; i < team.length; i++) {
     const thisMember = team[i];
+    const name = thisMember.name;
+    const role = thisMember.role;
+    const image = thisMember.image;
+    generateCardTemplate(name, role, image); // Metodo con il template literals
+    // generateCardAppend(name, role, image); // Metodo con append
+}
+
+/* FUNCTIONS */
+
+// Funzione che genera un elemento del DOM che rappresenta una card (metodo con template literals)
+// name: elemento che rappresenta la parola chiave dell'oggetto
+// role: elemento che rappresenta la parola chiave dell'oggetto
+// image: elemento che rappresenta la parola chiave dell'oggetto
+// return: elemento del dom che rappresenta una un cotainer con le card
+function generateCardTemplate(name, role, image) {
+    const cardContainer = document.querySelector('#card-container');
     cardContainer.innerHTML += `
-    <div class="col-9 mx-auto col-md-6 col-lg-4" >
+    <div class="col-9 col-md-6 col-lg-4 mx-auto" >
         <div class="card">
-            <img src="${thisMember.image}" class="card-img-top">
+            <img src="${image}" class="card-img-top">
             <div class="card-body text-center">
-                <div>${thisMember.name}</div>
-                <div>${thisMember.role}</div>
+                <div>${name}</div>
+                <div>${role}</div>
             </div>
         </div>
     </div >`
+    return cardContainer;
+}
+
+// Funzione che genera un elemento del DOM che rappresenta una card (metodo con create element)
+// name: elemento che rappresenta la parola chiave dell'oggetto
+// role: elemento che rappresenta la parola chiave dell'oggetto
+// image: elemento che rappresenta la parola chiave dell'oggetto
+// return: elemento del dom che rappresenta una colonna con una card
+function generateCardAppend(name, role, image) {
+    const col = document.createElement('div');
+    const card = document.createElement('div');
+    const img = document.createElement('img');
+    const cardBody = document.createElement('div');
+    const cardName = document.createElement('div');
+    const cardrole = document.createElement('div');
+    col.classList.add('col-9');
+    col.classList.add('col-md-6');
+    col.classList.add('col-lg-4');
+    col.classList.add('mx-auto');
+    card.classList.add('card');
+    img.src = image;
+    img.classList.add('card-img-top');
+    cardBody.classList.add('card-body');
+    cardBody.classList.add('text-center');
+    cardName.innerText = name;
+    cardrole.innerText = role;
+    cardContainer.append(col);
+    col.append(card);
+    card.append(img, cardBody);
+    cardBody.append(cardName, cardrole);
+    return col;
 }
